4727ff8b54f23650ac7a67ea93561c2fb431011f
wbin666 <wbin666@hotmail.com>
2016-05-18 18:51:12 +0800

    refined the repeatOptions to make 'daily' as default selected option

--- frontend/js/langExApp.js
+++ frontend/js/langExApp.js
@@ -16,4 +16,26 @@ angular.module('langExApp').controller('mainCtrl', ['$scope', function($scope){
 
     $scope.endDateLabel="To";
     $scope.endDateModel= new Date("2016-08-18");
+
+    $scope.repeatOptionSelected = "daily";
+}]);
+
+angular.module('langExApp').controller('datepickerInlineCtrl', ['$scope', function ($scope) {
+    $scope.today = function() {
+        $scope.dateSelected = new Date();
+    };
+    $scope.today();
+
+    $scope.options = {
+        //customClass: getDayClass,
+        minDate: new Date(),
+        showWeeks: true
+    };
+
+    // // Disable weekend selection
+    // function disabled(data) {
+    //     var date = data.date,
+    //         mode = data.mode;
+    //     return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    // }
 }]);
